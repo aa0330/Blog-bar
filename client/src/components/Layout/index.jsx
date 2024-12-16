16136bef1e5cdaceb2659bd0fc0e6de002699b9d
王超凡 <wangchaofan@terminus.io>
2024-12-16 20:14:03 +0800

    feat: sub scss file again

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer";
 import NavBar from "../Navbar";
+import  './index.scss'
 
 const Layout = () => {
   return (
--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './index.scss'
+
 const NavBar = () => {
   return (
     <div className="navbar">
