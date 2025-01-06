ef09b86d7cc5c1bf661887e6c0058b666a125e35
王武侯 <3144216129@qq.com>
2025-01-06 23:10:13 +0800

    feat:db

--- api/db.js
+++
@@ -1,7 +0,0 @@
-import mysq from "mysql";
-
-export const db = mysq.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-});
--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from 'cors'
+
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
-app.use(express.json());
+
+app.get('/login', (req, res) => {
+
+})
+
+
+
 app.listen(5173, () => {
   console.log("Connected!");
 });
