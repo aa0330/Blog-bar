0c8e5a473e850b6c90e6744b59580bf49f21d3b0
王超凡 <wangchaofan@terminus.io>
2025-02-14 18:02:54 +0800

    feat:请求修改

--- client/src/utils/request.ts
+++ client/src/utils/request.ts
@@ -1,154 +1,58 @@
 import axios from "axios";
-import { message } from "antd";
 
-type Fn = (data: FcResponse<any>) => unknown;
-
-interface IAnyObj {
-  [index: string]: unknown;
-}
-
-interface FcResponse<T> {
-  errno: string;
-  errmsg: string;
-  data: T;
-}
-
-const get = <T>(
-  url: string,
-  params: IAnyObj = {},
-  clearFn?: Fn
-): Promise<[any, FcResponse<T> | undefined]> =>
-  new Promise((resolve) => {
-    axios
-      .get(url, { params })
-      .then((result) => {
-        let res: FcResponse<T>;
-        if (clearFn !== undefined) {
-          res = clearFn(result.data) as unknown as FcResponse<T>;
-        } else {
-          res = result.data as FcResponse<T>;
-        }
-        resolve([null, res as FcResponse<T>]);
-      })
-      .catch((err) => {
-        resolve([err, undefined]);
-      });
-  });
-
-const handleRequestHeader = (config) => {
-  config[""];
-};
-
-const handleAuth = (config) => {
-  //   config.header["token"] = localStorage.getItem("token") || token || "";
-  return config;
-};
-
-axios.interceptors.request.use((config: any) => {
-  config = handleRequestHeader(config);
-  config = handleAuth(config);
-  return config;
+const request = axios.create({
+  baseURL: "", // 基础 URL
+  timeout: 10000, // 超时时间
+  headers: { "Content-Type": "application/json" }, // 默认请求头
 });
 
-const handleNetworkError = (errStatus) => {
-  let errMessage = "未知错误";
-  if (errStatus) {
-    switch (errStatus) {
-      case 400:
-        errMessage = "";
-        break;
-      case 401:
-        errMessage = "未授权，请重新登录";
-        break;
-      case 403:
-        errMessage = "拒绝访问";
-        break;
-      case 404:
-        errMessage = "请求错误,未找到该资源";
-        break;
-      case 405:
-        errMessage = "请求方法未允许";
-        break;
-      case 408:
-        errMessage = "请求超时";
-        break;
-      case 500:
-        errMessage = "服务器端出错";
-        break;
-      case 501:
-        errMessage = "网络未实现";
-        break;
-      case 502:
-        errMessage = "网络错误";
-        break;
-      case 503:
-        errMessage = "服务不可用";
-        break;
-      case 504:
-        errMessage = "网络超时";
-        break;
-      case 505:
-        errMessage = "http版本不支持该请求";
-        break;
-      default:
-        errMessage = `其他连接错误 --${errStatus}`;
+request.interceptors.request.use(
+  (config) => {
+    // 示例：自动添加 Token
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-  } else {
-    errMessage = "无法连接到服务器！";
-  }
-  message.error(errMessage);
-};
-const handleAuthError = (errno) => {
-  const authErrMap: any = {
-    "10031": "登录失效，需要重新登录", // token 失效
-    "10032": "您太久没登录，请重新登录~", // token 过期
-    "10033": "账户未绑定角色，请联系管理员绑定角色",
-    "10034": "该用户未注册，请联系管理员注册用户",
-    "10035": "code 无法获取对应第三方平台用户",
-    "10036": "该账户未关联员工，请联系管理员做关联",
-    "10037": "账号已无效",
-    "10038": "账号未找到",
-  };
-
-  if (authErrMap.hasOwnProperty(errno)) {
-    message.error(authErrMap[errno]);
-    // 授权错误，登出账户
-    // logout();
-    return false;
-  }
-
-  return true;
-};
-
-// const handleGeneralError = (errno, errmsg) => {
-//   if (err.errno !== "0") {
-//     meessage.error(err.errmsg);
-//     return false;
-//   }
-
-//   return true;
-// };
-
-axios.interceptors.response.use(
-  (response) => {
-    if (response.status !== 200) return Promise.reject(response.data);
-
-    handleAuthError(response.data.errno);
-    // handleGeneralError(response.data.errno, response.data.errmsg);
-
-    return response;
+    return config;
   },
-  (err) => {
-    handleNetworkError(err.response.status);
-    Promise.reject(err.response);
+  (error) => {
+    return Promise.reject(error);
   }
 );
 
-const defaultConfig = {
-  timeout: 10000,
-  
-};
+request.interceptors.response.use(
+  (response) => {
+    // 示例：统一处理响应数据格式
+    if (response.status === 200) {
+      return response.data; // 直接返回业务数据
+    } else {
+      return Promise.reject(response);
+    }
+  },
+  (error) => {
+    // 统一错误处理（网络错误、HTTP 状态码错误等）
+    let errorMessage = "请求失败";
+    if (error.response) {
+      // HTTP 状态码非 2xx 的情况
+      switch (error.response.status) {
+        case 401:
+          errorMessage = "未授权，请重新登录";
+          break;
+        case 404:
+          errorMessage = "请求资源不存在";
+          break;
+        case 500:
+          errorMessage = "服务器错误";
+          break;
+      }
+    } else if (error.request) {
+      // 请求已发出但无响应（如网络断开）
+      errorMessage = "网络连接异常";
+    }
+    // 全局错误提示（可接入 UI 框架的提示组件）
+    console.error(errorMessage);
+    return Promise.reject(error);
+  }
+);
 
-class Http {
-  constructor(porps) {}
-}
+export default request;
